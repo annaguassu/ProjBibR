54f01efe62280f07613fdb14b5469a7d89701929
ANA LOPES <ana.lopes28@fatec.sp.gov.br>
2022-05-25 12:16:29 -0300

    25-05-2022

--- ProjBibR/wwwroot/js/site.js
+++ ProjBibR/wwwroot/js/site.js
@@ -1,4 +1,22 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let stars = document.getElementById('stars')
+let moon = document.getElementById('moon')
+let mountains_behind = document.getElementById('mountains_behind')
+let text = document.getElementById('text')
+let btn = document.getElementById('btn')
+let mountains_front = document.getElementById('mountains_front')
 
-// Write your JavaScript code.
+
+window.addEventListener('scroll', function () {
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountains_behind.style.top = value * 0.5 + 'px';
+    mountains_front.style.top = value * 0 + 'px';
+    text.style.marginRight = value * 4 + 'px'
+    text.style.marginTop = value * 1.5 + 'px'
+    btn.style.marginTop = value * 1.5 + 'px'
+
+})
+/*
+
+*/
